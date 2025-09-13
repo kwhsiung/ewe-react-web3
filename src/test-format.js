50d6bb8a99554f88c5f73bbb12259667a445f988
Test User <test@example.com>
2025-09-13 21:16:11 +0800

    Test pre-commit hook with newly added poorly formatted code

--- src/test-format.js
+++ src/test-format.js
@@ -1,8 +1,15 @@
 // This is a test file with intentionally poor formatting
-function badlyFormatted(param1, param2) {
-  const result = param1 + param2;
-  return result;
+function   badlyFormatted(  param1,param2   ){
+      const result=param1+param2;
+          return      result   ;
 }
 
-const obj = { key1: "value1", key2: "value2" };
-console.log(obj);
+const   obj={key1:"value1",key2:"value2"};
+console.log(   obj    );
+
+// Adding another poorly formatted function
+function    anotherFunction(   a   ,   b,c    ){
+let   x  =  a+b;
+   let y=c*2;
+        return x+y;
+}
