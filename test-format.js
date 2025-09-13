79d817e071cdfef6b8d8bacc0cfa23133e0ee154
Test User <test@example.com>
2025-09-13 21:15:10 +0800

    Test pre-commit hook with poorly formatted file

---
+++ test-format.js
@@ -0,0 +1,8 @@
+// This is a test file with intentionally poor formatting
+function   badlyFormatted(  param1,param2   ){
+      const result=param1+param2;
+          return      result   ;
+}
+
+const   obj={key1:"value1",key2:"value2"};
+console.log(   obj    );
