28e72aff50f6d1dc018b3aadb61d5eb928884e05
kwhsiung <kaiwenhsiung@gmail.com>
2025-09-21 16:30:01 +0800

    Remove unused indexedDBStorage implementation from wagmi.ts to streamline code and improve maintainability.

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,21 +1,6 @@
-import { del, get, set } from "idb-keyval";
 import { createConfig, http } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
 import { metaMask, walletConnect } from "wagmi/connectors";
-
-// biome-ignore lint/correctness/noUnusedVariables: allowed
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const indexedDBStorage = {
-  async getItem(name: string) {
-    return get(name);
-  },
-  async setItem(name: string, value: string) {
-    await set(name, value);
-  },
-  async removeItem(name: string) {
-    await del(name);
-  },
-};
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
